7f60595d217fde47bcadd9760bffb8073bf4f4d5
youssefhelmy869 <youssefahmed1050@gmail.com>
2025-09-30 18:50:17 +0300

    draw pixsel added

--- func.hpp
+++ func.hpp
@@ -2,23 +2,70 @@
 #include <vector>
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "output_screen.hpp"
+#include <bitset>
+#pragma once
 
 using namespace sf;
 using namespace std;
-
-Color calculate_colour(int red, int green, int blue, int alpha)
+class termainl
 {
-    if (red < 0 || red > 255 ||
-        green < 0 || green > 255 ||
-        blue < 0 || blue > 255 ||
-        alpha < 0 || alpha > 255)
+private:
+    template <size_t size>
+    string decemial_to_binary(int num)
     {
-        cerr << "Couler value is bigger or equal to 256" << endl;
+        bitset<size> binary(num);
+        string return_value = binary.to_string();
+        return return_value;
     }
-    Color color(red, green, blue, alpha);
-    return color;
-}
 
-void excute_binary(string command)
-{
-}
+    int binary_to_decimal(string number)
+    {
+        int retrun_value = stoul(number, nullptr, 2);
+        return retrun_value;
+    }
+
+    Color calculate_colour(int red, int green, int blue, int alpha)
+    {
+        if (red < 0 || red > 255 ||
+            green < 0 || green > 255 ||
+            blue < 0 || blue > 255 ||
+            alpha < 0 || alpha > 255)
+        {
+            cerr << "Couler value is bigger or equal to 256" << endl;
+        }
+        Color color(red, green, blue, alpha);
+        return color;
+    }
+    screen scrn;
+    void draw_pixsel(string command)
+    {
+        string x_pos = command.substr(4, 16);
+        string y_pos = command.substr(20, 16);
+        string s_red = command.substr(36, 8);
+        string s_green = command.substr(44, 8);
+        string s_blue = command.substr(52, 8);
+        string s_alpha = command.substr(60, 8);
+
+        int red, green, blue, alpha;
+        red = binary_to_decimal(s_red);
+        green = binary_to_decimal(s_green);
+        blue = binary_to_decimal(s_blue);
+        alpha = binary_to_decimal(s_alpha);
+
+        Color c = calculate_colour(red, green, blue, alpha);
+        int x = binary_to_decimal(x_pos);
+        int y = binary_to_decimal(y_pos);
+        scrn.add_pixel(x, y, c);
+    }
+
+public:
+    void excute_binary(string command)
+    {
+        string main_command = command.substr(0, 4);
+        if (main_command == "0000")
+        {
+            draw_pixsel(command);
+        }
+    }
+};
---
+++ output_screen.hpp
@@ -0,0 +1,207 @@
+/// the run() takes the main thread while the rest of the mian that run() was called in will be excuted by a logic_thread
+
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <string>
+#include <iostream>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+
+class screen
+{
+private:
+    RenderWindow *window = nullptr;
+    Image image;
+    Texture texture;
+    Sprite sprite;
+    mutex mtx;
+    thread logic_thread;
+    bool update_image = false;
+    bool runing;
+    int x;
+    int y;
+    Font font;
+    Text user_input;
+
+    vector<Text> texts;
+
+    string buffer;
+    bool ready = false;
+    vector<Text> user_input_history;
+    int line_height = 28;
+    string last_input;
+
+    void handle_text_input(Event &event)
+    {
+        if (event.type == Event::TextEntered)
+        {
+            if (event.text.unicode == '\b')
+            {
+                if (!buffer.empty())
+                    buffer.pop_back();
+            }
+            else if (event.text.unicode == '\r' || event.text.unicode == '\n')
+            {
+                if (!buffer.empty())
+                {
+                    add_line(buffer);
+
+                    last_input = buffer;
+                    ready = true;
+                    buffer.clear();
+                }
+                ready = true;
+            }
+            else if (event.text.unicode < 128) // ASCII only
+            {
+                buffer += static_cast<char>(event.text.unicode);
+            }
+        }
+        user_input.setString(buffer);
+    }
+    void add_line(const string &str)
+    {
+        Text line(str, font, 20);
+        line.setFillColor(Color::White);
+        line.setPosition(10, 10 + user_input_history.size() * line_height);
+        user_input_history.push_back(line);
+    }
+
+public:
+    bool is_ready()
+    {
+        return ready;
+    }
+    string getInput()
+    {
+        if (ready == true)
+        {
+            ready = false;
+            return last_input;
+        }
+        else{
+            return "";
+        }
+    }
+    string get_buffer()
+    {
+        return buffer;
+    }
+
+    bool window_is_open;
+
+private:
+    void create_window()
+    {
+
+        // start loop
+
+        window_is_open = true;
+
+        while (window->isOpen() && runing == true)
+        {
+            window->clear();
+            Event user_event;
+
+            while (window->pollEvent(user_event))
+            {
+                if (user_event.type == Event::Closed)
+                {
+                    window->close();
+                    lock_guard<mutex> lock(mtx);
+                    window_is_open = false;
+                }
+                handle_text_input(user_event);
+            }
+
+            if (update_image == true)
+            {
+                texture.update(image);
+                update_image = false;
+            }
+
+            window->draw(sprite);
+            lock_guard<mutex> lock(mtx);
+            if (!user_input_history.empty())
+            {
+                window->draw(user_input_history.back());
+            }
+
+
+            for (const auto &t : texts)
+            {
+                window->draw(t);
+            }
+            window->draw(user_input);
+
+            window->display();
+        }
+        window_is_open = false;
+    }
+
+public:
+    void write_to_window(string data, int x_pos, int y_pos, int size = 20)
+    {
+        Text text(data, font, size);
+        text.setFont(font);
+        text.setFillColor(Color::White);
+        text.setPosition(x_pos, y_pos);
+        lock_guard<mutex> lock(mtx);
+        texts.push_back(text);
+    }
+
+    screen(int para_x = 800, int para_y = 800)
+    {
+        x = para_x;
+        y = para_y;
+        window = new RenderWindow(VideoMode(x, y), "main window");
+        image.create(x, y);
+        texture.loadFromImage(image);
+        sprite.setTexture(texture);
+        if (!font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf"))
+        {
+            cerr << "File not found" << endl;
+        }
+        user_input.setFont(font);
+        user_input.setCharacterSize(24);
+        user_input.setFillColor(Color::Green);
+        user_input.setPosition(10, y - 40); // bottom of window
+    }
+
+    void add_pixel(int x_position, int y_postion, Color colour = Color::White)
+    {
+
+        cout << "adding pixsel" << endl;
+        image.setPixel(x_position, y_postion, colour);
+        update_image = true;
+    }
+
+    template <typename Func>
+    void run(Func rest_of_main)
+    {
+        cout << "run function called" << endl;
+        runing = true;
+        cout << "logic thread created" << endl;
+        logic_thread = thread([&, rest_of_main]()
+                              { rest_of_main(); });
+
+        create_window();
+
+        if (logic_thread.joinable())
+        {
+            logic_thread.join();
+        }
+    }
+
+    ~screen()
+    {
+        runing = false;
+        if (window && window->isOpen())
+            window->close();
+        if (logic_thread.joinable())
+            logic_thread.join();
+    }
+};
